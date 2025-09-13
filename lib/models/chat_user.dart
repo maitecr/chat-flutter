d978a2584147fce00fa7c44b682ce37ad7ce0e7a
Maite <maicapref@gmail.com>
2025-09-13 11:11:19 -0300

    inicio do desenvolvimento de autenticação de usuário

---
+++ lib/models/chat_user.dart
@@ -0,0 +1,15 @@
+class ChatUser {
+
+  final String id;
+  final String username;
+  final String name;
+  final String email;
+
+  const ChatUser({
+    required this.id,
+    required this.username,
+    required this.name,
+    required this.email
+  })
+
+}
